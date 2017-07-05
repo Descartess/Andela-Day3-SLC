4cb5de1bd42a47db10c391a504e16dbb24869b04
NyondoPaul <nyondopaul23@gmail.com>
2017-07-05 09:40:56 +0300

    add PEP8 styles

--- findMinMax/tests.py
+++ findMinMax/tests.py
@@ -6,20 +6,20 @@ class TestMinMax(unittest.TestCase):
     """ methods for testing find_min_max"""
     def test_positive(self):
         """ test return values for all positive"""
-        minmax = find_min_max([1,2,4,8,])
-        self.assertEqual([1,8],minmax)
+        minmax = find_min_max([1, 2, 4, 8])
+        self.assertEqual([1, 8], minmax)
     def test_negative(self):
         """ test return values for all negative"""
-        minmax = find_min_max([-1,-2,-4,-8,])
-        self.assertEqual([-8,-1],minmax)
+        minmax = find_min_max([-1, -2, -4, -8])
+        self.assertEqual([-8, -1], minmax)
     def test_two_values(self):
         """test return two values """
-        minmax = find_min_max([4,1])
-        self.assertEqual([1,4],minmax)
+        minmax = find_min_max([4, 1])
+        self.assertEqual([1, 4], minmax)
     def test_same_value(self):
         """test return for same value"""
-        minmax = find_min_max([4,4,4,4])
-        self.assertEqual([4],minmax)
+        minmax = find_min_max([4, 4, 4, 4])
+        self.assertEqual([4], minmax)
 
 
 
