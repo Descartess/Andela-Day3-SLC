6489a122546b09937d3ab0208b3e0b6e29dc31b6
NyondoPaul <nyondopaul23@gmail.com>
2017-07-05 10:07:44 +0300

    code refactor

--- words/words.py
+++ words/words.py
@@ -1,10 +1,11 @@
-def words(string):
-  """ takes string and returns occurances for each word """
-  string_list = string.split()
-  output = {}
-  for string in set(string_list):
-    if string.isdigit():
-      output[int(string)] = string_list.count(string)
-    else:
-      output[string] = string_list.count(string)
-  return output
+""" words function takes string and returns occurances for each word """
+def words(long_string):
+    """ takes string and returns occurances for each word """
+    string_list = long_string.split()
+    output = {}
+    for string in set(string_list):
+        if string.isdigit():
+            output[int(string)] = string_list.count(string)
+        else:
+            output[string] = string_list.count(string)
+    return output
