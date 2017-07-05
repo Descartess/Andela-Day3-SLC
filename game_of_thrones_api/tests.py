963023edb896e5e73f281ede51439f1b6f49b4a9
NyondoPaul <nyondopaul23@gmail.com>
2017-07-05 15:10:15 +0300

    Add mock testing

--- game_of_thrones_api/tests.py
+++ game_of_thrones_api/tests.py
@@ -1,10 +1,18 @@
 """ unit tests for game of thrones api """
 import unittest
-from mock import Mock
-from game_of_thrones import main
+import requests
+from mock import patch, Mock
+
+from game_of_thrones import get_character,get_house
 
 class GameofThronesTest(unittest.TestCase):
-    def test_example(self):
-        self.assertTrue(True)
+    """unit tests for public api code """
+    def test_get_character(self):
+        """ test api call to get characters """
+        with patch.object(requests, 'get') as get_mock:
+            get_mock.return_value = mock_response = Mock()
+            mock_response.status_code = 200
+            self.assertEqual(200, mock_response.status_code)
+
 if __name__ == "__main__":
     unittest.main()
