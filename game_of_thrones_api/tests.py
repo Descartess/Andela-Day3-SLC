5a29313a6f67658cd737747a8f1ca8bfc95396e2
NyondoPaul <nyondopaul23@gmail.com>
2017-07-05 12:49:14 +0300

    Added tests

---
+++ game_of_thrones_api/tests.py
@@ -0,0 +1,10 @@
+""" unit tests for game of thrones api """
+import unittest
+from mock import Mock
+from game_of_thrones import main
+
+class GameofThronesTest(unittest.TestCase):
+    def test_example(self):
+        self.assertTrue(True)
+if __name__ == "__main__":
+    unittest.main()
